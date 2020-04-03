28920517da5f23e69e3a73737bd98fd6939af3c2
Oleg Sapun <oleg.sapun@gmail.com>
2020-04-03 14:55:58 +0300

    feat: add new function convertToStringFrom

--- RSSchool_T3/RSSchool_T3/1/PolynomialConverter.m
+++ RSSchool_T3/RSSchool_T3/1/PolynomialConverter.m
@@ -2,6 +2,49 @@
 
 @implementation PolynomialConverter
 - (NSString*)convertToStringFrom:(NSArray <NSNumber*>*)numbers {
-    return @"";
+
+
+    NSMutableString *str = [[NSMutableString alloc] init];
+    BOOL skipNext = false;
+
+    if ([numbers count] == 0) {
+        return nil;
+    } else {
+        NSUInteger countNumbers = [numbers count];
+        for (int i = 0; i < [numbers count]; i++) {
+            NSNumber *num = numbers[i];
+
+            countNumbers = countNumbers - 1;
+            if (skipNext) {
+                skipNext = false;
+            } else {
+                if (countNumbers >= 2) {
+                    if (abs([num intValue]) != 1) {
+                        [str appendString:[NSString stringWithFormat:@"%d%@%lu",abs([num intValue]), @"x^", (unsigned long)(countNumbers)]];
+                    } else {
+                        [str appendString:[NSString stringWithFormat:@"%@%lu", @"x^", (unsigned long)(countNumbers)]];
+                    }
+                } else if (countNumbers == 1) {
+                    if (abs([num intValue]) != 1) {
+                        [str appendString:[NSString stringWithFormat:@"%d%@",abs([num intValue]), @"x"]];
+                    } else {
+                        [str appendString:[NSString stringWithFormat:@"%@", @"x"]];
+                    }
+                } else if (countNumbers == 0) {
+                    [str appendString:[NSString stringWithFormat:@"%d",abs([num intValue])]];
+                }
+            }
+            if (countNumbers > 0) {
+                if ([numbers[i+1] intValue] > 0) {
+                    [str appendString:[NSString stringWithFormat:@" + "]];
+                } else if ([numbers[i+1] intValue] < 0){
+                    [str appendString:[NSString stringWithFormat:@" - "]];
+                } else {
+                    skipNext = true;
+                }
+            }
+        }
+    }
+    return str;
 }
 @end
